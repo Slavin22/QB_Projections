51b0622c3bb918d00c2c14db0352238f0a1dbb66
Sean Slavin <sean@Seans-MacBook-Pro.local>
2017-11-14 18:43:17 -0500

    Added files

---
+++ script.js
@@ -0,0 +1,31 @@
+var trace1 = {
+	x: qbData.map(row => row.adjPA),
+	y: qbData.map(row => row.adjPF),
+	mode: 'markers',
+	type: 'scatter',
+	text: matchups,
+	marker: {size: 20, bordercolor: 'black'},
+	hoverinfo: 'text',
+	hoverlabel: {bgcolor: 'orange', bordercolor: 'black'},
+	hoveron: {bgcolor: 'orange'},
+	fillcolor: "black"
+}
+
+var data = [trace1];
+
+var layout = {
+  xaxis: {
+    range: [4.5, 25],
+    title: 'Defense - Adjusted QB Points Allowed'
+  },
+  yaxis: {
+    range: [4.5, 25],
+    title: 'QB - Adjusted Points Scored'
+  },
+  title:'Week 11 QB Projections',
+  hovermode: 'closest'
+};
+
+Plotly.newPlot("plot", data, layout)
+
+renderTable()
